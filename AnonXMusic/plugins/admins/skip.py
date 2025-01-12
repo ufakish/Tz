cf9ba4ad5a13381947c25f814c4df605544a0a02
ufakish <phalishort@gmail.com>
2025-01-12 07:14:37 +0530

    Update skip.py

--- AnonXMusic/plugins/admins/skip.py
+++ AnonXMusic/plugins/admins/skip.py
@@ -219,7 +219,7 @@ async def skip(cli, message: Message, _, chat_id):
             db[chat_id][0]["markup"] = "tg"
         else:
             button = stream_markup(_, chat_id)
-            img = await get_thumb(videoid)
+            img = await get_thumb(videoid,user_id)
             run = await message.reply_photo(
                 photo=img,
                 caption=_["stream_1"].format(
